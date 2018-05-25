4c89f2a81c512fd6295b7fd383a230722ea58229
batherit <batherit0703@naver.com>
2018-05-25 19:01:23 +0900

    Revert "원시 명령 커맨드 구현"
    This reverts commit ebfc317e278090b305393926c409106935380bab.

--- Code/PrevTestLab/PrevTestLab/main.cpp
+++ Code/PrevTestLab/PrevTestLab/main.cpp
@@ -1,49 +1,16 @@
 #include<iostream>
-#include<conio.h>
 #include"CGameTimer.h"
 using namespace std;
 
-CGameTimer test_timer(TIMER_TYPE_WINDOWS);
-
-char GetKey(void) {
-	// 키보드 입력 감지를 true, false로 반환
-	if (_kbhit()) {
-		return _getch();
-	}
-	return '\0';
-}
-
-void HandleCommand(char& ch) {
-	switch (ch) {
-	case 'r': case 'R':
-		cout << "reset" << endl;
-		test_timer.Reset();
-		break;
-	case 'p': case 'P':
-		cout << "pause" << endl;
-		test_timer.RunToPause();
-		break;
-	case 's': case 'S':
-		cout << "start" << endl;
-		test_timer.PauseToRun();
-		break;
-	case 't': case 'T':
-		cout << "print result" << endl;
-		float time = test_timer.GetElapsedTimePerFrame();
-		cout << "time per frame "<< time << "sec " << 1.0f / time << "fps" << endl;
-		time = test_timer.GetTotalTimeFromRun();
-		cout << "total time " << time << "sec " << endl << endl;
-	}
-}
-
 int main(void){
-	bool is_paused = false;
-
-	// 게임 루프
+	CGameTimer test_timer(TIMER_TYPE_WINDOWS);
 	while (true) {
+		/*CCommand cmd = GetCommand();
+		handleCommand(cmd);
+		Update();
+		Render();*/
 		test_timer.RunTick();
-		char input = GetKey();
-		HandleCommand(input);
+		cout << test_timer.GetElapsedTimePerFrame() << "sec" << endl;
 	}
 	return 0;
 }
